9b211451a4bc04da4ae052213d1adc53e0249809
Julien ROSAMOND <jrosamon@e1r10p12.42.fr>
2017-01-17 18:41:22 +0100

    c partit

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ let server = http.createServer(app).listen(3000);
 let path = require('path');
 
 let index = require('./routes/index');
+let connexion = require('./routes/connexion');
+let inscription = require('./routes/inscription');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use('/', index);
+app.use('/connexion', connexion);
+app.use('/inscription', inscription);
 
 module.exports = app;
